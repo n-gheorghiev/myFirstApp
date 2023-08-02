3b3c7db43ee30615c0a45f27821c150f7e738525
ncheorghiev <nicolai.gheorghiev@iis.utm.md>
2023-08-02 22:55:40 +0300

    Initial Commit

---
+++ myApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myApp
+//
+//  Created by Home on 02.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
