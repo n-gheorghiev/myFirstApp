b98561b2cc1d1ad90443cd6c52c0b983c9c4b92e
ncheorghiev <nicolai.gheorghiev@iis.utm.md>
2023-08-06 17:30:45 +0300

    Lesson 15 (Arrays)

---
+++ myApp/SvetoforView.swift
@@ -0,0 +1,89 @@
+//
+//  SvetoforView.swift
+//  myApp
+//
+//  Created by Home on 06.08.2023.
+//
+
+import UIKit
+import SnapKit
+
+class SvetoforView: UIView {
+    
+    private let sections: [SvetoforSectionView] = [SvetoforSectionView(colorLight: .red),
+                                                   SvetoforSectionView(colorLight: .red),
+                                                   SvetoforSectionView(colorLight: .blue),
+                                                   SvetoforSectionView(colorLight: .red),
+                                                   SvetoforSectionView(colorLight: .yellow),
+                                                   SvetoforSectionView(colorLight: .red)]
+    
+    private let stackView = UIStackView(frame: .zero)
+    
+    init() {
+        super.init(frame: .zero)
+        
+        configureStackView()
+        setStartState()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Public
+    
+    func switchMode() {
+        // Если все секции не горят, то зажигаем первую
+        // если секция горит, то мы её выключаем и зажигаем следующую
+        
+        if let indexOfFirstOnSection = sections.firstIndex(where: { $0.isOn() }), indexOfFirstOnSection < sections.count - 1 {
+            // Секция горит
+            sections[indexOfFirstOnSection].turnOff()
+            sections[indexOfFirstOnSection + 1 ].turnOn()
+        } else {
+            // все секции не горят
+//            sections[0].turnOn()
+            sections.forEach({ $0.turnOff() })
+            sections.first?.turnOn()
+        }
+    }
+    
+    // MARK: - Private
+    
+    private func configureStackView() {
+        addSubview(stackView)
+        
+        stackView.spacing = 12
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        
+        sections.forEach { section in
+            stackView.addArrangedSubview(section)
+            
+            section.snp.makeConstraints { make in
+                make.width.height.equalTo(100)
+            }
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    private func setStartState() {
+//        firstSectionView.turnOff()
+//        secondSectionView.turnOff()
+//        thirdSectionView.turnOff()
+
+//        for section in sections {
+//            section.turnOff()
+//        }
+//
+//        sections.forEach { section in
+//            section.turnOff()
+//        }
+        
+        sections.forEach({ $0.turnOff() })
+    }
+}
